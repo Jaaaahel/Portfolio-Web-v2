5929358e0edddb4cc95b86ede0bff3df1749b519
Jeremiah <jahelrelova@outlook.com>
2019-05-15 23:14:28 +0800

    Added path to views

--- src/router.js
+++ src/router.js
@@ -10,6 +10,31 @@ export default new Router({
       path: '/',
       name: 'home',
       component: () => import('./views/Home.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('./views/About.vue')
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('./views/Projects.vue')
+    },
+    {
+      path: '/experience',
+      name: 'experience',
+      component: () => import('./views/Experience.vue')
+    },
+    {
+      path: '/websites',
+      name: 'websites',
+      component: () => import('./views/Websites.vue')
+    },
+    {
+      path: '/apps',
+      name: 'apps',
+      component: () => import('./views/Apps.vue')
     }
   ]
 })
