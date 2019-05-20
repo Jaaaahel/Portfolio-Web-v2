1ddac26380cde4dfef42ced16a573c76814232a8
Jeremiah <jahelrelova@outlook.com>
2019-05-20 20:58:24 +0800

    Added about page

---
+++ src/views/About.vue
@@ -0,0 +1,51 @@
+<template>
+	<section class="hero is-fullheight-with-navbar has-background-white" style="overflow: hidden;">
+		<div class="hero-body">
+			<div class="container has-text-black">
+				<h1 class="title is-size-1 has-text-weight-bold has-text-black has-text-centered" style="margin-top: 16px;" data-aos="fade-right" data-aos-delay="150" data-aos-duration="500">
+					ABOUT
+				</h1>
+				<hr class="has-background-black hrabout" style="width: 10%; display:block; margin: auto; margin-top: -20px; margin-bottom: 32px; border: 2px solid black; border-radius: 5px;" data-aos="fade-left" data-aos-delay="500" data-aos-duration="500">
+				<div class="columns is-centered">
+					<div class="column has-text-centered" data-aos="fade-right" data-aos-delay="140" data-aos-duration="500">
+						<div>
+							<figure class="image" style="width: 65%; display:block; margin: auto;">
+								<img src="@/assets/img/me.png">
+							</figure>
+						</div>
+						<br>
+						<div>
+							<h2 class="subtitle is-size-3 has-text-black">
+								Who am I?
+							</h2>
+							<h3 class="subtitle is-size-4 has-text-black">
+								I'm Jahel, an aspiring Developer. I just graduated in STI College Novaliches and currently looking for a job.
+							</h3>
+						</div>
+					</div>
+					<div class="column" data-aos="fade-left" data-aos-delay="140" data-aos-duration="500">
+						<h2 class="subtitle is-size-4 has-text-black" style="margin-bottom: -10px;">
+							I know about:
+						</h2>
+						<div class="content is-size-4 has-text-left">
+							<ul class="is-inline-block">
+								<li type="disc">HTML, CSS, JavaScript</li>
+								<li type="disc">Bulma CSS Framework</li>
+								<li type="disc">jQuery</li>
+								<li type="disc">Vue.js</li>
+								<li type="disc">PHP</li>
+								<li type="disc">MySQL</li>
+								<li type="disc">WordPress</li>
+								<li type="disc">Java</li>
+								<li type="disc">XML</li>
+								<li type="disc">C#</li>
+								<li type="disc">Adobe Photoshop</li>
+								<li type="disc">Adobe Premiere</li>
+							</ul>	
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
