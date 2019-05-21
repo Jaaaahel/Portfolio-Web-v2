11621be9a580974259a05c6d691138f52bb5aa42
Jeremiah <jahelrelova@outlook.com>
2019-05-21 19:22:05 +0800

    Added projects page

---
+++ src/views/Projects.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<section class="hero is-fullheight has-background-black" id="portfolio-v2" style="overflow: hidden;">
+			<div class="hero-body">
+				<div class="container has-text-centered">
+					<h1 class="title is-size-1 has-text-weight-bold has-text-white has-text-centered" style="margin-top: 16px;" data-aos="fade-right" data-aos-delay="150" data-aos-duration="500">
+						PROJECTS
+					</h1>
+					<hr class="has-background-black hrabout" style="width: 15%; display:block; margin: auto; margin-top: -20px; margin-bottom: 32px; border: 2px solid white; border-radius: 5px;" data-aos="fade-left" data-aos-delay="500" data-aos-duration="500">
+
+					<router-link class="button is-medium is-rounded has-text-white is-success" to="/websites" style="width: 30%; margin-right: 15px;">Web Projects</router-link>
+					<router-link class="button is-medium is-rounded has-text-white is-danger" to="/apps" style="width: 30%;">Android Projects</router-link>
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
