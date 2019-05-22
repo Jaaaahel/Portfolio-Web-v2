c323396c53cc5f19e83555a460797ec4f6e6e12f
Jeremiah <jahelrelova@outlook.com>
2019-05-22 21:40:36 +0800

    Added websites page

---
+++ src/views/Websites.vue
@@ -0,0 +1,200 @@
+<template>
+	<div>
+		<section class="hero is-fullheight has-background-black" id="portfolio-v2" style="overflow: hidden;">
+			<div class="hero-body">
+				<div class="container">
+					<h1 class="title is-size-1 has-text-weight-bold has-text-white has-text-centered" style="margin-top: 16px;" data-aos="fade-right" data-aos-delay="150" data-aos-duration="500">
+						PROJECTS
+					</h1>
+					<hr class="has-background-black hrabout" style="width: 15%; display:block; margin: auto; margin-top: -20px; margin-bottom: 32px; border: 2px solid white; border-radius: 5px;" data-aos="fade-left" data-aos-delay="500" data-aos-duration="500">
+
+					<div class="has-text-centered" style="margin-bottom: 32px;">
+						<router-link class="button is-medium is-rounded has-text-white is-success" to="/websites" style="width: 30%; margin-right: 15px;">Web Projects</router-link>
+						<router-link class="button is-medium is-rounded has-text-white is-danger" to="/apps" style="width: 30%;">Android Projects</router-link>
+					</div>
+
+					<div class="columns is-centered">
+						<div class="column has-text-centered" data-aos="fade-right" data-aos-delay="140" data-aos-duration="500">
+							<div>
+								<h2 class="subtitle is-size-2 has-text-white has-text-weight-bold">
+									Portfolio v2
+								</h2>
+								<figure class="image" style="width: 100%; display:block; margin: auto;">
+									<img src="@/assets/img/portfolio-preview.png">
+								</figure>
+							</div>
+						</div>
+						<div class="column" data-aos="fade-left" data-aos-delay="140" data-aos-duration="500">
+							<h3 class="subtitle is-size-4 has-text-white" style="margin-top: 16px; text-align: justify;">
+								I made this website to showcase my projects and skills using Vue JS. The first version was made using jQuery.
+							</h3>
+							<h2 class="subtitle is-size-4 has-text-white" style="margin-bottom: -10px;">
+								In this project I used:
+							</h2>
+							<div class="content has-text-white is-size-4 has-text-left">
+								<ul class="is-inline-block">
+									<li type="disc">HTML, CSS, JavaScript</li>
+									<li type="disc">Bulma CSS Framework</li>
+									<li type="disc">Vue.js</li>
+									<li type="disc">Adobe Photoshop</li>
+								</ul>	
+							</div>
+							<div class="has-text-centered">
+								<a class="button is-medium is-rounded has-text-black is-white" href="jahelrelova.com" style="width: 50%;">Visit Website</a>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<section class="hero is-fullheight has-background-black" id="portfolio" style="overflow: hidden;">
+			<div class="hero-body">
+				<div class="container">
+					<div class="columns is-centered">
+						<div class="column has-text-centered" data-aos="fade-right" data-aos-delay="140" data-aos-duration="500">
+							<div>
+								<h2 class="subtitle is-size-2 has-text-white has-text-weight-bold">
+									Portfolio
+								</h2>
+								<figure class="image" style="width: 100%; display:block; margin: auto;">
+									<img src="@/assets/img/portfolio-preview.png">
+								</figure>
+							</div>
+						</div>
+						<div class="column" data-aos="fade-left" data-aos-delay="140" data-aos-duration="500">
+							<h3 class="subtitle is-size-4 has-text-white" style="margin-top: 16px; text-align: justify;">
+								I made this website to showcase my projects and skills using Vue JS. The first version was made using jQuery.
+							</h3>
+							<h2 class="subtitle is-size-4 has-text-white" style="margin-bottom: -10px;">
+								In this project I used:
+							</h2>
+							<div class="content has-text-white is-size-4 has-text-left">
+								<ul class="is-inline-block">
+									<li type="disc">HTML, CSS, JavaScript</li>
+									<li type="disc">Bulma CSS Framework</li>
+									<li type="disc">Vue.js</li>
+									<li type="disc">Adobe Photoshop</li>
+								</ul>	
+							</div>
+							<div class="has-text-centered">
+								<a class="button is-medium is-rounded has-text-black is-white" href="v1.jahelrelova.com" style="width: 50%;">Visit Website</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="hero is-fullheight has-background-black" id="portfolio" style="overflow: hidden;">
+			<div class="hero-body">
+				<div class="container">
+					<div class="columns is-centered">
+						<div class="column has-text-centered" data-aos="fade-right" data-aos-delay="140" data-aos-duration="500">
+							<div>
+								<h2 class="subtitle is-size-2 has-text-white has-text-weight-bold">
+									Virtual Assistant Portfolio
+								</h2>
+								<figure class="image" style="width: 100%; display:block; margin: auto;">
+									<img src="@/assets/img/portfolio-preview.png">
+								</figure>
+							</div>
+						</div>
+						<div class="column" data-aos="fade-left" data-aos-delay="140" data-aos-duration="500">
+							<h3 class="subtitle is-size-4 has-text-white" style="margin-top: 16px; text-align: justify;">
+								I made this website to showcase my projects and skills using Vue JS. The first version was made using jQuery.
+							</h3>
+							<h2 class="subtitle is-size-4 has-text-white" style="margin-bottom: -10px;">
+								In this project I used:
+							</h2>
+							<div class="content has-text-white is-size-4 has-text-left">
+								<ul class="is-inline-block">
+									<li type="disc">HTML, CSS, JavaScript</li>
+									<li type="disc">Bulma CSS Framework</li>
+									<li type="disc">Vue.js</li>
+									<li type="disc">Adobe Photoshop</li>
+								</ul>	
+							</div>
+							<div class="has-text-centered">
+								<a class="button is-medium is-rounded has-text-black is-white" href="virtualassistantph.jahelrelova.com" style="width: 50%;">Visit Website</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="hero is-fullheight has-background-black" id="portfolio" style="overflow: hidden;">
+			<div class="hero-body">
+				<div class="container">
+					<div class="columns is-centered">
+						<div class="column has-text-centered" data-aos="fade-right" data-aos-delay="140" data-aos-duration="500">
+							<div>
+								<h2 class="subtitle is-size-2 has-text-white has-text-weight-bold">
+									EyeServe-Web
+								</h2>
+								<figure class="image" style="width: 100%; display:block; margin: auto;">
+									<img src="@/assets/img/portfolio-preview.png">
+								</figure>
+							</div>
+						</div>
+						<div class="column" data-aos="fade-left" data-aos-delay="140" data-aos-duration="500">
+							<h3 class="subtitle is-size-4 has-text-white" style="margin-top: 16px; text-align: justify;">
+								I made this website to showcase my projects and skills using Vue JS. The first version was made using jQuery.
+							</h3>
+							<h2 class="subtitle is-size-4 has-text-white" style="margin-bottom: -10px;">
+								In this project I used:
+							</h2>
+							<div class="content has-text-white is-size-4 has-text-left">
+								<ul class="is-inline-block">
+									<li type="disc">HTML, CSS, JavaScript</li>
+									<li type="disc">Bulma CSS Framework</li>
+									<li type="disc">Vue.js</li>
+									<li type="disc">Adobe Photoshop</li>
+								</ul>	
+							</div>
+							<div class="has-text-centered">
+								<a class="button is-medium is-rounded has-text-black is-white" href="eyeserve.jahelrelova.com" style="width: 50%;">Visit Website</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="hero is-fullheight has-background-black" id="portfolio" style="overflow: hidden;">
+			<div class="hero-body">
+				<div class="container">
+					<div class="columns is-centered">
+						<div class="column has-text-centered" data-aos="fade-right" data-aos-delay="140" data-aos-duration="500">
+							<div>
+								<h2 class="subtitle is-size-2 has-text-white has-text-weight-bold">
+									Faces
+								</h2>
+								<figure class="image" style="width: 100%; display:block; margin: auto;">
+									<img src="@/assets/img/portfolio-preview.png">
+								</figure>
+							</div>
+						</div>
+						<div class="column" data-aos="fade-left" data-aos-delay="140" data-aos-duration="500">
+							<h3 class="subtitle is-size-4 has-text-white" style="margin-top: 16px; text-align: justify;">
+								I made this website to showcase my projects and skills using Vue JS. The first version was made using jQuery.
+							</h3>
+							<h2 class="subtitle is-size-4 has-text-white" style="margin-bottom: -10px;">
+								In this project I used:
+							</h2>
+							<div class="content has-text-white is-size-4 has-text-left">
+								<ul class="is-inline-block">
+									<li type="disc">HTML, CSS, JavaScript</li>
+									<li type="disc">Bulma CSS Framework</li>
+									<li type="disc">Vue.js</li>
+									<li type="disc">Adobe Photoshop</li>
+								</ul>	
+							</div>
+							<div class="has-text-centered">
+								<a class="button is-medium is-rounded has-text-black is-white" href="faces.jahelrelova.com" style="width: 50%;">Visit Website</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
